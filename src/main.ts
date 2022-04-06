c2033653b69ebd33d2fbde26afb6f46f3b90fbde
Shin <tristan.spinnewyn@ik.me>
2022-04-07 00:10:57 +0200

    fix & add role for login

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,20 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const whitelist = [
+    'http://localhost:3001',
+    'https://mangatheque.netlify.app/',
+  ];
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
   await app.listen(3000);
 }
 bootstrap();
