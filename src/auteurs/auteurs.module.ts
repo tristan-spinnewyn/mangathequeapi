4893c6766da6828583faa5704ee67ef3fceae35d
tristan <tristan.spinnewyn@outlook.fr>
2022-06-08 16:22:58 +0200

    fix module auteurs

--- src/auteurs/auteurs.module.ts
+++ src/auteurs/auteurs.module.ts
@@ -1,4 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { AuteursController } from './auteurs.controller';
+import { AuteursService } from './auteurs.service';
+import { AuthMiddleware } from '../auth/middleware/auth.middleware';
 
-@Module({})
-export class AuteursModule {}
+@Module({
+  controllers: [AuteursController],
+  providers: [AuteursService],
+  exports: [AuteursService],
+})
+export class AuteursModule implements NestModule {
+  public configure(consumer: MiddlewareConsumer): any {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes(
+        { path: 'auteurs', method: RequestMethod.POST },
+        { path: 'auteurs/:id', method: RequestMethod.PUT },
+      );
+  }
+}
